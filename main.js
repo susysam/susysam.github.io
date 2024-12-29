26e6c9e2d6087216e1acd6df9d424a1d180b4dc2
susysam <124097175+susysam@users.noreply.github.com>
2024-12-29 17:48:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// Button Event Listeners
+document.getElementById('tabButton').addEventListener('click', function () {
+  const url = document.getElementById('websiteName').value;
+  if (url) {
+    window.open(url, '_blank');
+  } else {
+    alert('Please enter a valid website URL!');
+  }
+});
 
+document.getElementById('windowButton').addEventListener('click', function () {
+  const url = document.getElementById('websiteName').value;
+  if (url) {
+    window.open(url, '_blank', 'width=800,height=600');
+  } else {
+    alert('Please enter a valid website URL!');
+  }
+});
